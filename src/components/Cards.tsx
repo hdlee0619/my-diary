00b742e3c9a968a17dbe64b58cbfb79563124a1e
hdlee0619 <dlgusehd1211@naver.com>
2023-02-18 15:25:03 +0900

    Design: Card component 생성 및 구조 생성
    SCSS로 구조만 간단하게 생성, 기능 먼저 구현 후 에 css 수정할 예정

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Cards = () => {
-  return <div>Cards</div>;
+  return (
+    <div className="card">
+      <h1>Title</h1>
+      <div>
+        <span>date</span>
+        <span>weather</span>
+        <span>author</span>
+      </div>
+      <div>
+        <button>수정</button>
+        <button>삭제</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
